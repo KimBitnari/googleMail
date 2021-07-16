6873287fe9bad806d9d702f9ce32559ca0e3fba8
KimBitnari-hgu <21700099@handong.edu>
2021-07-16 17:34:31 +0900

    edut layout

--- components/mailLayout.js
+++ components/mailLayout.js
@@ -13,90 +13,90 @@ export default function Layout({ children, inbox, starred, sent, trash }) {
                 <title>Gmail</title>
             </Head>
             <header>
-                <div class="p-2 flex">
-                    <div class="pr-8 h-12 flex min-w-max" style={{ width:"238px" }}>
-                        <div class="p-3 mx-1 inline-block align-middle cursor-pointer"><IoMenu size="30"/></div>
-                        <div class="relative top-1 float-left">
+                <div className="p-2 flex">
+                    <div className="pr-8 h-12 flex min-w-max" style={{ width:"238px" }}>
+                        <div className="p-3 mx-1 inline-block align-middle cursor-pointer"><IoMenu size="30"/></div>
+                        <div className="relative top-1 float-left">
                             <Link href="/">
-                                <a class="inline-block align-middle">
+                                <a className="inline-block align-middle">
                                 <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
                                     style={{ width:"129px" }} />
                                 </a>
                             </Link>
                         </div>
                     </div>
-                    <div class="flex h-14 min-w-max flex-auto px-2">
-                        <div class="h-8 px-2.5 w-full">
-                            <div class="bg-gray-100 relative rounded h-14" style={{ maxWidth:"720px" }}>
-                                <div class="p-2 m-1 inline-block float-left cursor-pointer"><HiSearch size="30" /></div>
-                                <div class="block relative float-left top-4"><input placeholder="메일 검색" class="bg-transparent" /></div>
-                                <div class="p-2 m-1 right-0 cursor-pointer absolute"><IoMdOptions size="30" /></div>
+                    <div className="flex h-14 min-w-max flex-auto px-2">
+                        <div className="h-8 px-2.5 w-full">
+                            <div className="bg-gray-100 relative rounded h-14" style={{ maxWidth:"720px" }}>
+                                <div className="p-2 m-1 inline-block float-left cursor-pointer"><HiSearch size="30" /></div>
+                                <div className="block relative float-left top-4"><input placeholder="메일 검색" className="bg-transparent" /></div>
+                                <div className="p-2 m-1 right-0 cursor-pointer absolute"><IoMdOptions size="30" /></div>
                             </div>
                         </div>
                     </div>
-                    <div class="min-w-max">
-                        <div class="p-2 m-1 inline-block cursor-pointer"><IoMdHelpCircleOutline size="30" /></div>
-                        <div class="p-2 m-1 inline-block cursor-pointer"><IoSettingsOutline size="30" /></div>
+                    <div className="min-w-max">
+                        <div className="p-2 m-1 inline-block cursor-pointer"><IoMdHelpCircleOutline size="30" /></div>
+                        <div className="p-2 m-1 inline-block cursor-pointer"><IoSettingsOutline size="30" /></div>
                     </div>
-                    <div class="min-w-max">
-                        <div class="p-2 m-1 inline-block cursor-pointer"><IoIosKeypad size="30" /></div>
-                        <div class="pt-1 px-2 mx-1 inline-block float-right cursor-pointer"><div class="flex items-center justify-center w-10 h-10 rounded-full bg-purple-700 text-white">김</div></div>
+                    <div className="min-w-max">
+                        <div className="p-2 m-1 inline-block cursor-pointer"><IoIosKeypad size="30" /></div>
+                        <div className="pt-1 px-2 mx-1 inline-block float-right cursor-pointer"><div className="flex items-center justify-center w-10 h-10 rounded-full bg-purple-700 text-white">김</div></div>
                     </div>
                 </div>
             </header>
             <hr />
             <main>
-                <div class="h-full float-left" style={{ width: "256px" }}>
-                    <div class="flex shadow-md rounded-full h-14 py-2 px-3 w-36 cursor-pointer ml-3 my-4">
-                        <img class="absolute" src="https://www.gstatic.com/images/icons/material/colored_icons/2x/create_32dp.png" width="42" />
-                        <p class="my-2 ml-12">편지쓰기</p>
+                <div className="h-full float-left" style={{ width: "256px" }}>
+                    <div className="flex shadow-md rounded-full h-14 py-2 px-3 w-36 cursor-pointer ml-3 my-4">
+                        <img className="absolute" src="https://www.gstatic.com/images/icons/material/colored_icons/2x/create_32dp.png" width="42" />
+                        <p className="my-2 ml-12">편지쓰기</p>
                     </div>
                     <div>
-                        {inbox ? (<div class="bg-gray-200 rounded-r-full mr-4">
-                                <div class="ml-6 h-8">
-                                    <IoFileTraySharp class="absolute text-gray-500" size="24" />
-                                    <p class="my-2 ml-12 text-gray-500">받은편지함</p>
+                        {inbox ? (<div className="bg-gray-200 rounded-r-full mr-4">
+                                <div className="ml-6 h-8">
+                                    <IoFileTraySharp className="absolute text-gray-500" size="24" />
+                                    <p className="my-2 ml-12 text-gray-500">받은편지함</p>
                                 </div>
-                            </div>) : (<div class="mr-4">
-                                <div class="ml-6 h-8">
-                                    <IoFileTraySharp class="absolute text-gray-500" size="24" />
-                                    <p class="my-2 ml-12 text-gray-500">받은편지함</p>
-                                </div>
-                            </div>)
-                        }
-                        {starred ? (<div class="bg-gray-200 rounded-r-full mr-4">
-                                <div class="ml-6 h-8">
-                                    <IoStarSharp class="absolute text-gray-500" size="24" />
-                                    <p class="my-2 ml-12 text-gray-500">별표편지함</p>
-                                </div>
-                            </div>) : (<div class="mr-4">
-                                <div class="ml-6 h-8">
-                                    <IoStarSharp class="absolute text-gray-500" size="24" />
-                                    <p class="my-2 ml-12 text-gray-500">별표편지함</p>
+                            </div>) : (<div className="mr-4">
+                                <div className="ml-6 h-8">
+                                    <IoFileTraySharp className="absolute text-gray-500" size="24" />
+                                    <p className="my-2 ml-12 text-gray-500">받은편지함</p>
                                 </div>
                             </div>)
                         }
-                        {sent ? (<div class="bg-gray-200 rounded-r-full mr-4">
-                                <div class="ml-6 h-8">
-                                    <IoMdSend class="absolute text-gray-500" size="24" />
-                                    <p class="my-2 ml-12 text-gray-500">보낸편지함</p>
+                        {starred ? (<div className="bg-gray-200 rounded-r-full mr-4">
+                                <div className="ml-6 h-8">
+                                    <IoStarSharp className="absolute text-gray-500" size="24" />
+                                    <p className="my-2 ml-12 text-gray-500">별표편지함</p>
                                 </div>
-                            </div>) : (<div class="mr-4">
-                                <div class="ml-6 h-8">
-                                    <IoMdSend class="absolute text-gray-500" size="24" />
-                                    <p class="my-2 ml-12 text-gray-500">보낸편지함</p>
+                            </div>) : (<div className="mr-4">
+                                <div className="ml-6 h-8">
+                                    <IoStarSharp className="absolute text-gray-500" size="24" />
+                                    <p className="my-2 ml-12 text-gray-500">별표편지함</p>
                                 </div>
                             </div>)
                         }
-                        {trash ? (<div class="bg-gray-200 rounded-r-full mr-4">
-                                <div class="ml-6 h-8">
-                                    <FaTrash class="absolute text-gray-500" size="24" />
-                                    <p class="my-2 ml-12 text-gray-500">휴지통</p>
+                        {sent ? (<div className="bg-gray-200 rounded-r-full mr-4">
+                                <div className="ml-6 h-8">
+                                    <IoMdSend className="absolute text-gray-500" size="24" />
+                                    <p className="my-2 ml-12 text-gray-500">보낸편지함</p>
                                 </div>
-                            </div>) : (<div class="mr-4">
-                                <div class="ml-6 h-8">
-                                    <FaTrash class="absolute text-gray-500" size="24" />
-                                    <p class="my-2 ml-12 text-gray-500">휴지통</p>
+                            </div>) : (<div className="mr-4">
+                                <div className="ml-6 h-8">
+                                    <IoMdSend className="absolute text-gray-500" size="24" />
+                                    <p className="my-2 ml-12 text-gray-500">보낸편지함</p>
+                                </div>
+                            </div>)
+                        }
+                        {trash ? (<div className="bg-gray-200 rounded-r-full mr-4">
+                                <div className="ml-6 h-8">
+                                    <FaTrash className="absolute text-gray-500" size="24" />
+                                    <p className="my-2 ml-12 text-gray-500">휴지통</p>
+                                </div>
+                            </div>) : (<div className="mr-4">
+                                <div className="ml-6 h-8">
+                                    <FaTrash className="absolute text-gray-500" size="24" />
+                                    <p className="my-2 ml-12 text-gray-500">휴지통</p>
                                 </div>
                             </div>)
                         }
--- pages/sampleApi/index.js
+++ pages/sampleApi/index.js
@@ -4,14 +4,14 @@ import 'tailwindcss/tailwind.css'
 
 export default function Home({ data }) {
     return (
-    <div class="mx-10 mt-2">
+    <div className="mx-10 mt-2">
         <Head>
             <title>Books</title>
         </Head>
         <section>
-            <h2 class="mb-2 font-bold">Categories</h2>
+            <h2 className="mb-2 font-bold">Categories</h2>
             {data.landing_course_categories.map((category) => (
-                <div class="float-left w-1/5 border-yellow-500 bg-yellow-500 text-center rounded-md mx-2" key={category.id}>
+                <div className="float-left w-1/5 border-yellow-500 bg-yellow-500 text-center rounded-md mx-2" key={category.id}>
                     {category.title}
                 </div>
             ))}
@@ -20,23 +20,23 @@ export default function Home({ data }) {
         <hr />
         <br />
         <section>
-            <h2 class="mb-2 font-bold">Books</h2>
-            <table class="border-collapse">
+            <h2 className="mb-2 font-bold">Books</h2>
+            <table className="border-collapse">
                 <tr>
-                    <th class="text-center bg-yellow-500 bg-opacity-25 p-4">category</th>
-                    <th class="text-center bg-yellow-500 bg-opacity-25 p-4">image</th>
-                    <th class="text-center bg-yellow-500 bg-opacity-25 p-4">title</th>
-                    <th class="text-center bg-yellow-500 bg-opacity-25 p-4">sub-title</th>
-                    <th class="text-center bg-yellow-500 bg-opacity-25 p-4">date</th>
+                    <th className="text-center bg-yellow-500 bg-opacity-25 p-4">category</th>
+                    <th className="text-center bg-yellow-500 bg-opacity-25 p-4">image</th>
+                    <th className="text-center bg-yellow-500 bg-opacity-25 p-4">title</th>
+                    <th className="text-center bg-yellow-500 bg-opacity-25 p-4">sub-title</th>
+                    <th className="text-center bg-yellow-500 bg-opacity-25 p-4">date</th>
                 </tr>
                 {data.landing_courses.map((landCoures) => (
                     landCoures.courses.map((course) => (
                         <tr key={course.id} class="divide-y">
-                            <td class="p-4 text-xs">{course.category}</td>
-                            <td class="p-4"><img src={course.image_url} alt={course.title} style={{ width:"50px" }} /></td>
-                            <td class="p-4">{course.title}</td>
-                            <td class="p-4 text-xs">{course.subtitle}</td>
-                            <td class="p-4 text-xs">{course.created_at}</td>
+                            <td className="p-4 text-xs">{course.category}</td>
+                            <td className="p-4"><img src={course.image_url} alt={course.title} style={{ width:"50px" }} /></td>
+                            <td className="p-4">{course.title}</td>
+                            <td className="p-4 text-xs">{course.subtitle}</td>
+                            <td className="p-4 text-xs">{course.created_at}</td>
                         </tr>
                     ))
                 ))}
